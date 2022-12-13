c42c7e775a76377e1047cfaf264a7c523c6bede4
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2022-12-13 16:32:51 +0800

    upda

--- production/vite.config.js
+++ production/vite.config.js
@@ -2,16 +2,22 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import { splitVendorChunkPlugin } from "vite";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+
+import { compression } from "vite-plugin-compression2";
 export default defineConfig({
   plugins: [
+    compression(),
+    ViteMinifyPlugin({}),
     splitVendorChunkPlugin(),
+
     legacy({
       targets: ["defaults", "not IE 11"],
       modernPolyfills: true,
     }),
   ],
   build: {
-    manifest:true,
+    manifest: true,
     rollupOptions: {
       input: {
         app: resolve(__dirname, "/index.html"),
