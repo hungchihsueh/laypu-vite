30b5f3e73fdb2f17ce1e2b9e9be6c1c75ef42b25
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2022-12-15 19:11:12 +0800

    update

--- production/vite.config.js
+++ production/vite.config.js
@@ -3,12 +3,17 @@ import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import { splitVendorChunkPlugin } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
-
+import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   plugins: [
     ViteMinifyPlugin({}),
+    // chunkSplitPlugin(),
     splitVendorChunkPlugin(),
-
+   visualizer({
+        open:true,  //注意这里要设置为true，否则无效
+        gzipSize:true,
+        brotliSize:true
+    }),
     legacy({
       targets: ["defaults", "not IE 11"],
       modernPolyfills: true,
